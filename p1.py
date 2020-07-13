d5570a4154e80c1abb7f43c144228d4eaaa15efc
Benjamin CONTE <benjamin.conte@outlook.fr>
2020-07-13 16:07:37 +0200

    dog class

--- p1.py
+++ p1.py
@@ -1,3 +1,17 @@
-yoyo = 2
-yiyi = 6
-yaya = 9
+class Dog:
+    species = "canis familiaris"
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"{self.name} is {self.age} years old"
+
+    def speak(self, sound):
+        return f"{self.name} says {sound}"
+
+buddy = Dog("buddy", 5)
+miles = Dog("miles", 6)
+
+buddy.speak('WOOF')
+
